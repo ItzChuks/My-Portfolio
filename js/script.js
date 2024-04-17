93c2e08e28aa775be0f0443cc34c6ac29e8ef1d6
ItzChuks <Itzchuks1@gmail.com>
2024-04-17 14:06:10 +0100

    commited

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* typing animation */
+var typed = new Typed(".typing", {
+    strings:["Web Designer","Web Developer","UI-UX Designer","Graphic Designer"],
+    typeSpeed: 50,
+    BackSpeed: 50,
+    loop:true
+});
+
