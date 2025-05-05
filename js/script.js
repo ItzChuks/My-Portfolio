d25ce5a6f9ab44e78c1ca78e3abfeeb9770b8902
ItzChuks <163852451+ItzChuks@users.noreply.github.com>
2025-05-05 16:06:56 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* typing animation */
 var typed = new Typed(".typing", {
-    strings:["Web Designer","Web Developer","UI-UX Designer","Graphic Designer"],
+    strings:["Web Designer","Web Developer","UI-UX Designer","Mobile Developer"],
     typeSpeed: 50,
     BackSpeed: 50,
     loop:true
